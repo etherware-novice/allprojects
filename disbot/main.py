6918cbe7362427f55715dcb7511dafc8a3e7cb4a
texan-down-under <reesesimonasher@gmail.com>
2021-03-03 02:19:34 +0000

    added the var for bot api

--- disbot/main.py
+++ disbot/main.py
@@ -1,5 +1,9 @@
 import discord
 import os
+from keepalive import keepalive
+
+DISCORD_BOT_SECRET=
+
 
 client = discord.Client()
 
@@ -13,5 +17,7 @@ async def on_message(message):
     if message.author != client.user:
       await message.channel.send(message.content[::-1])
     
+
+keep_alive()
 token = os.environ.get("DISCORD_BOT_SECRET")
 client.run(token)
