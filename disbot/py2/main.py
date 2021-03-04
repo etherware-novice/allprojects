bf71a9ec0ab6367112a47b55be3fa28125cf9595
texan-down-under <73374039+etherware-novice@users.noreply.github.com>
2021-03-04 17:44:58 -0600

    what even is a dotenv?

--- disbot/py2/main.py
+++ disbot/py2/main.py
@@ -2,15 +2,15 @@
 import os
 
 import discord
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load.dotenv()
-TOKEN = os.getenv('ODE2ODI5NzkzMjIzNTczNTY0.YEAqBg.kHVrz3qaknP8EDBOWN9KySMBDds')
+#load.dotenv()
+TOKEN = 'ODE2ODI5NzkzMjIzNTczNTY0.YEAqBg.kHVrz3qaknP8EDBOWN9KySMBDds'
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{Client.user} has connected to discord!')
+    print(f'{client.user} has connected to discord!')
 
 client.run(TOKEN)
