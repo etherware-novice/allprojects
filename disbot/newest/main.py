472de93a8921952ad4a2a1fd547008bf7385ac3d
texan-down-under <73374039+etherware-novice@users.noreply.github.com>
2021-04-09 22:53:38 -0500

    push of the work stuff ive done

--- disbot/newest/main.py
+++ disbot/newest/main.py
@@ -7,21 +7,31 @@ def main():
 	import asyncio
 	from datetime import datetime
 	from dotenv import load_dotenv
-	intents = discord.Intents.default()
-	intents.members = True
+	import traceback
+	intents = discord.Intents.default() #sets up the intents obj
+	intents.members = True #flips the member inperwhatever it is to true
 
 	load_dotenv()
-	TOKEN = os.getenv('DISCORD_TOKEN')
+	TOKEN = os.getenv('DISCORD_TOKEN') #retrieves the stuff needed from a *hidden* env file yall aint gettin
 	GUILD = os.getenv('DISCORD_GUILD')
 
-	bot = commands.Bot(command_prefix='!', intents=intents)
+	bot = commands.Bot(command_prefix='a!', intents=intents) #initilizes the bot
 
-	channel = bot.get_channel("825935386684686346")
+	channel = bot.get_channel("825935386684686346") #log channel where everything is sent
+
+	
+	@commands.command()
+	async def load(self, *, module): #a thing i copied thats supposed to display module errors but it doesnt work that well
+		"""Loads a module."""
+		try: 
+			self.bot.load_extension(module)
+		except Exception:
+			e = discord.Embed(description=traceback.format_exc())
 
 	@bot.event
-	async def on_ready():
+	async def on_ready(): #initilization
 
-	    guild = discord.utils.get(bot.guilds, name=GUILD)
+	    guild = discord.utils.get(bot.guilds, name=GUILD) #gets the guild that i put in to kinda add some flavor text
 
 	    print(
 	    	f'{bot.user} is connected to the following guild:\n'
@@ -31,28 +41,33 @@ def main():
 
 
 
-	    await bot.change_presence(activity=discord.Game(name='candycane\'s bot :)'))
+	    await bot.change_presence(activity=discord.Game(name='candycane\'s bot :)')) #:)
 
 
 
-	@bot.command(name='reload', help='Reloads the python commands')
+	@bot.command(name='reload', help='Reloads the python commands') #special command that accesses this repo and pulls/switches branches (this is why upd.py exists)
 	async def relbot(ctx, state=0):
-		bot = ctx.bot
-		channel = bot.get_channel(825935386684686346)
+		bot = ctx.bot #re-attains the bot obj but i dont think i use it here oops
+		channel = bot.get_channel(825935386684686346) #im not 10000% sure how namespace works here so just incase
 		
-		brn = "HEAD"
-		if state == 1: brn = "master"
+		brn = "HEAD" #sets the branch to HEAD so the checkout command below doesn't error if a state isnt input
+		if state == 1: brn = "master" # switches which branch is being checked out
 		elif state == 2: brn = "discordbotnew3andknuckles"
 
-		os.system(f"git checkout {brn}")
-		os.system(f"git pull origin")
+		os.system(f"git checkout {brn}") #switch branch
+		os.system(f"git pull origin") #and re pulls it
 
-		print(f'[{ctx.guild.name}]: User [{ctx.author}] used [restart] command on state {state}')
-		await channel.send(f'[{ctx.guild.name}]: User [{ctx.author}] used [restart] command on state {state}')
-		await ctx.send(f'{ctx.author} reloaded the bot.')
+		print(f'[{ctx.guild.name}]: User [{ctx.author}] used [restart] command on state {state}') #log to the python file itself
+		await channel.send(f'[{ctx.guild.name}]: User [{ctx.author}] used [restart] command on state {state}') #log to log channel
+		await ctx.send(f'{ctx.author} reloaded the bot.') #flavor text sent to the channel that triggered it
 
-		bot.reload_extension("upd")
+		bot.reload_extension("upd") #reloads upd.py
 
-	bot.load_extension("upd")
-	bot.run(TOKEN)
-main()
+	bot.load_extension("upd") #init load of upd.py
+	bot.run(TOKEN) #runs everything else
+
+	
+
+main() #runs everything else
+
+#dont ask why everything is in a main func
--- disbot/newest/upd.py
+++ disbot/newest/upd.py
@@ -8,6 +8,7 @@ import random
 from discord.ext import commands
 import asyncio
 from datetime import datetime
+import json
 from dotenv import load_dotenv
 
 # todo: add self.log channel
@@ -17,43 +18,110 @@ from dotenv import load_dotenv
 class General(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-		
+
+		print("abc") #check to if the thing is starting up
+
+		self.weemcnt = jsonfile('0') #json files used initilizer (look at jsonfile below for more info)
+		self.testmr = jsonfile('ignore.json')
+		self.mdb = jsonfile('msgwm.json')
 
 
-		print("abc")
 
-	@commands.Cog.listener()
-	async def on_member_join(self, member):
-		print("tst")
-		await member.send(
+	#simple dm user on join function
+	@commands.Cog.listener() 
+	async def on_member_join(self, member): #when a member joins:
+		await member.send( #member.send is send a dm with this info
 				f'Hi {member.name}, welcome to my test server!'
 			)
 
+
+
+
+	#random num generator
 	@commands.command(name='rng', help='Responds with a random number')
 	#now = timeup("%H:%M")
-	async def randreturn(self, ctx):
-		response =  random.randint(0, 500)
-		await ctx.send(response)
+	async def randreturn(self, ctx): #function used here
+		response =  random.randint(0, 500) #generates a number
+		await ctx.send(response) #sends to the same channel the command was entered
 		#await ctx.send('new')
-		
-		await self.log(ctx, "rng")
+		#await asyncio.ensure_future(self.testmr.astim(ctx, 15, 60)) #sets a timer for 15 minutes (15 seconds * 60)
+		await self.log(ctx, "rng") #calls log func
 
+
+
+
+	#a dnd dice generator
 	@commands.command(name='dndr', help="Returns a certain number of dice with modifiers")
 	async def dnd(self, ctx, size:int, num:int, mod:int):
-		dice = [random.randint(1, size) for _ in range(num)]
+		dice = [random.randint(1, size) for _ in range(num)] #i think this is getting the dice roll nums idk how this works
 
 		numsum = 0
 		for d in dice:
-			numsum += d
+			numsum += d #adds up all the dice
 		
-		await ctx.send(dice)
+		await ctx.send(dice) #neat little flavor text im proud of it
 		await ctx.send(f'{numsum} + {mod}')
 		numsum += mod
 		await ctx.send(numsum)
 		
-		await self.log(ctx, "dnd dice")
+		await self.log(ctx, "dnd dice") #logs it
 
 
+	#tester using other user
+	@commands.command(name='usr', help='testing a command with a separate user')
+	async def usr(self, ctx, user: discord.Member):
+		await ctx.send(await (self.chkrl(ctx, user))) #calls for checkrole function with input user
+		await ctx.send(await (self.chkrl(ctx, ctx.message.author))) #call for checkrole func with the member who triggered 
+		ctx.message.author = user
+
+
+
+	#time to use everything ive set up to make weem spamming a thing of the past
+	@commands.Cog.listener()
+	#await self.chkrl(ctx, user)
+	#weemcnt.data[user]
+	#weemcnt.
+
+
+
+
+	#this was a testing program i just kinda left in to play with
+	@commands.command(name='dbt', help='input a number to increase/decrease your individual database entry')
+	async def dbe(self, ctx, mod:int): #function
+		#usr = f"{ctx.author.display_name} - {ctx.author.discriminator}" #gets the name of the user in readabl
+		usr = ctx.author.name
+
+		try: num = self.mdb.data[usr] #attempts to retrieve the existing data
+		except: num = 0 #fallback data/init num
+
+		num += mod #modifies the number with the input 
+		if num > 9999999999999999999999999999999999999999999999999999999999999999999: await ctx.send(f"{ctx.author}, the number is way too big") #hard limit instead of the bit limit
+		else:
+			await ctx.send(num) #returns num
+			self.mdb.edit(usr, num) #modifies the user's databawse file
+			await self.log(ctx, "mini database edit") #log log log
+
+		#print(f'{ctx.author.id} ({ctx.author.display_name} - {ctx.author.discriminator})')
+
+
+
+
+	#mini asyncronous timer test
+	@commands.command(name='time', help='testing async timers')
+	async def time(self, ctx, tm:int):
+		await ctx.send(f'initilizing timer for {tm} seconds') #flavor text
+		#await asyncio.ensure_future(self.testmr.astim(ctx, tm)) #the actual stuff
+		await ctx.send(f'timer for {tm} seconds done') #flavor text
+
+
+
+	#@commands.Cog.listener()
+	#async def on_message(self, message):
+
+
+
+
+	#integration ping blocker
 	@commands.Cog.listener()
 	async def on_message(self, message):
 
@@ -66,28 +134,114 @@ class General(commands.Cog):
 			#await message.channel.send(f"{message.author} has been weem blocked")
 			#await self.log(message, "weem", "the message was deleted")
 
-                try:
-                    if message.author.roles == 1: print('Check your code, this message should not appear')
+				try:
+					if message.author.roles == 1: print('Check your code, this message should not appear') #basically tries to accesss the sender's roles (which will always fail if its an integration)
                 
-                except:
-                    if '@' in message.content:
-                        await message.delete()
-                        await self.log(message, 'ping using the freaking tuppers', 'the message was deleted')
+				except:
+					if '@' in message.content: #checks if the @ sign is in the msg
+						await message.delete() #delete
+						await self.log(message, 'ping using the freaking tuppers', 'the message was deleted')
 
 
+# heres the manual utility commands used here
+
+	#log functs
 	async def log(self, ctx, cmd, act = "nothing happened"):
 		try:
-                        if ctx.me                     
-			msg = f'[{ctx.guild.name}]: User [{ctx.author}] used [{cmd}] command in #{ctx.channel}'
-		except:
-			msg = f'[{ctx.guild}]: User [{ctx.author}] sent [{cmd}] in #{ctx.channel} and {act}'
+			if ctx.me == 0: print('what is ctx me?') #checks if ctx.me exists which will always fail if <message> is used instead of <ctx>
+			msg = f'[{ctx.guild.name}]: User [{ctx.author}] used [{cmd}] command in #{ctx.channel}' #sets the string to be logged
+		except: #this will only be triggered if a messaege obj is imported
+			msg = f'[{ctx.guild}]: User [{ctx.author}] sent [{cmd}] in #{ctx.channel} and {act}' #sets the string to be logged
                         
 		
-		channel = self.bot.get_channel(825935386684686346)
+		channel = self.bot.get_channel(825935386684686346) #look i dont know how namespace works im just setting this var constantly
 		
-		print(msg)
-		await channel.send(msg)
+		print(msg) #outputs message to command line
+		await channel.send(msg) #sends the log string to the log channel
 
+	async def chkrl(self, ctx, user):
+		modrl = [ #list of roles that are "elevated"
+			"Admin",
+			"candycane"
+		]
+		blacklist = [ #list of member roles
+			"User",
+			"user",
+			813469324328173608
+		]
+
+		for x in modrl:
+			if isinstance(x, str): x = discord.utils.find(lambda r: r.name == x, ctx.guild.roles) #if its a string, use utils.find to get the role obj
+			elif isinstance(x, int): x = ctx.guild.get_role(x) #if not string, but is an int, simply use get_role to get role obj
+			if x in user.roles: return 1 #checks if the input user has the role from blacklist, if yes, returns -1
+		for x in blacklist: #for every entry in blacklist
+			if isinstance(x, str): x = discord.utils.find(lambda r: r.name == x, ctx.guild.roles) #if its a string, use utils.find to get the role obj
+			elif isinstance(x, int): x = ctx.guild.get_role(x) #if not string, but is an int, simply use get_role to get role obj
+			if x in user.roles: return -1 #checks if the input user has the role from blacklist, if yes, returns -1
+
+
+		return 0 #final fallback
+
+
+
+#json file related class
+
+class jsonfile(object):
+	"""docstring for jsonfile"""
+
+	def __init__(self, file:str = "0"): #file is an actual argument
+		super(jsonfile, self).__init__() #idk what this does but i feel like it might break if i remove
+		self.file = file #makes the argument file a class var
+
+		if file == "0": #alternate option to make it not write to file and reset on shutdown
+			self.data = {"null": 1} #sets the data class var up
+
+		else: #if a file was input
+			try: #attempts to load said file (will always fail if the file doesnt exist..yet)
+				with open(self.file) as file: #attempts to load the file that was inputted
+					self.data = json.load(file) # parses the JSON data in the file and returns a dictionary assigned to class var data
+			except: #if the file doesnt exist yet
+				try: 
+					file = file.name #IODataWrapper preventer
+				except:
+					file = file #throwaway except function so that except runs something
+
+					self.data = {"null": 1} #still makes sure data exists to not error out
+					self.write(self.data) #invokes the local write function to write out the file
+		self.timer = -1 #initilizes the timer class var
+
+
+	#overrides the json with input
+	def write(self, input): 
+		if self.file != "0": #checks if a file was input
+			with open(self.file, "w") as file: #opens up the file for writing
+				file.write(json.dumps(input, indent=4)) # formats and beautifies the JSON as a string and overwrites the file contents with it
+			self.data = input #updates the data var to make sure its uptodate
+
+	#local timer funct
+	@commands.Cog.listener()
+	async def astim(self, ctx, time, multi = 1):
+		time = time * multi #self-explanatory, added this so you dont have to calculate for minutes or hours and you can just change multi field
+		
+		
+		for x in range(time, 0, -1): #for loop counting down
+			self.timer = time #updates the timer variable
+			await asyncio.sleep(1) #wait for one second but asyncronously
+
+		self.timer = -1 #sets the timer to -1 denoting its inactive
+		self.edit(ctx.author, 0) #edits the original message trigger author's entry to 0
+	#async 	
+
+	#editing function
+	def edit(self, entry:str, input):
+		self.data[entry] = input #replaces the <entry> with input
+		self.write(self.data) #invokes write function
+
+	#a simpler version of write that just writes the current state of data to the file
+	def update(self):
+		self.write(self.data) #write function invoked
+
+#some boilerplate code that makes the cog functionable
 def setup(bot):
 	bot.add_cog(General(bot))
 	
