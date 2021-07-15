057da3d6804ee9cc873a82a0d05b5cb848b61d46
texan-down-under <73374039+etherware-novice@users.noreply.github.com>
2021-07-14 23:12:43 -0500

    this is pointless aa

---
+++ disbot/hell/main.py
@@ -0,0 +1,106 @@
+import os #import hell
+import discord
+import random
+from discord.ext import commands
+import asyncio
+from datetime import datetime
+from dotenv import load_dotenv
+import traceback
+import re
+import yaml
+import io
+import socket
+from discord.ext import tasks
+from datetime import datetime
+import requests
+from discord import Webhook, AsyncWebhookAdapter
+import aiohttp
+from dhooks import Webhook as dhook
+from aioconsole import ainput
+from colr import color
+from array import *
+import getpass
+import sys
+from PIL import Image
+
+
+intents = discord.Intents.all() #sets up the intents obj
+#intents.members = True #flips the member inperwhatever it is to true
+#intents.presences = True
+TOKEN = None
+cls = "\u001B[2J"
+
+
+#sys.stderr = open("err.txt", 'w')
+
+#load_dotenv()
+#TOKEN = os.getenv('DISCORD_TOKEN') #retrieves the stuff needed from a *hidden* env file yall aint gettin
+
+with open('token.yaml') as file:
+    documents = yaml.full_load(file)
+    try:
+        TOKEN = documents['token']
+    except KeyError:
+        raise KeyError(f'Token not found, please check the file {file}') #where the bot token comes from
+
+
+client = discord.Client(intents=intents) #initilizes the bot (its not a full bot bot bc i dont need the command stuff)
+client.token = TOKEN
+client.gif = im = Image.open("image0.gif")
+client.giflen = 0
+try:
+    while True:
+        im.seek(im.tell()+1)
+        client.giflen += 1
+except EOFError:
+    pass
+
+
+#flavorie text to make sure its working good
+@client.event
+async def on_ready(): #initilization
+
+    #flavor text
+    now = datetime.now()
+    curtime = now.strftime("%B %d, %Y at %H:%M")
+    print(f"\nHello, user <{getpass.getuser()}>. Today's datetime is {curtime}.")
+    print(f"\n{client.user} is connected to the following guilds:")
+    print(*[ #this little star means to decompress the list
+        f"{x.name} ({x.member_count} members)" #format of each entry
+        for x in client.guilds #getting each entry in the loop
+        ], sep = "\n") #making sure its on a newline
+
+global ovr
+ovr = 0
+@client.event
+async def on_message(message):
+    global ovr
+    #print(type(message.author.status))
+    if message.author.bot: return
+    rng = random.randint(1, 19)
+    print(rng)
+    if rng <= 1 or ovr:
+        if ovr: ovr = 0
+        if message.webhook_id != None:
+            ovr = 1
+            return
+        #get_image()
+        buffer = await asyncio.to_thread(imageget)
+
+        if random.randint(0, 1):
+            target = random.choice([
+                x for x in message.guild.members if x.raw_status in ("online", "dnd")
+            ])
+        else: target = message.author
+
+        await message.channel.send(f"{target.mention}, face the wheel of DOOOOM!", file=discord.File(buffer, filename="some_image.png"))
+
+def imageget():
+        buffer = io.BytesIO()
+        client.gif.seek(random.randint(0, client.giflen))
+        client.gif.save(buffer, format="PNG")
+        buffer.seek(0)
+        return buffer   
+
+
+client.run(TOKEN)
