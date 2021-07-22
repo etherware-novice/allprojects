73304f8befbfb54a050853d7e2cd8ad10c786c54
texan-down-under <73374039+etherware-novice@users.noreply.github.com>
2021-07-22 15:59:11 -0500

    stf

---
+++ python/gifmake.py
@@ -0,0 +1,37 @@
+import sys, getopt, os, re
+from PIL import Image
+
+path = os.path.abspath(f"{os.getcwd()}\{sys.argv[1]}")
+
+images = []
+args = [sys.argv[2]]
+
+if not os.path.exists(path):
+    path = os.getcwd()
+
+try:
+    args.append(sys.argv[3])
+except: pass
+
+    
+def search(pre:str = "", post:str = ""):
+    for x in range(0, 4):
+        y = str(0).rjust(x, "0")
+        if os.path.isfile(f"{path}\\{pre}{y}{post}"): 
+            n = 0
+            break
+    else: n = 1
+    pre = f".*{pre}" if pre != "" else pre
+    for x in os.listdir(path):
+        if re.match(f"^{pre}0*{n}{{1}}{post}.png", x) != None: 
+            yield x
+            n += 1
+        #if re.match(f"^{pre}", x): yield x
+
+
+for x in search(*args):
+    im = Image.open(f"{path}\{x}")
+    images.append(im)
+
+
+images[0].save('output.gif', format="gif", save_all=True, append_images=images[1:], loop=0, optimize = True, duration=41.66) #converts the list to a gif
