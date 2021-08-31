342c71f466472e25bf3ed477f2624f9207a56d3c
texan-down-under <73374039+etherware-novice@users.noreply.github.com>
2021-08-30 22:27:09 -0500

    Update maththing.py

--- python/maththing.py
+++ python/maththing.py
@@ -1,7 +1,7 @@
 import random
 from sympy import *
 
-def gennum():
+def getnum():
     for a in range(1, 10):
         for b in range(1, 10):
             if b == a: continue
@@ -16,10 +16,33 @@ def gennum():
                             for g in range(1, 10):
                                 if g in (a, b, c, d, e, f): continue
                                 yield (a, b, c, d, e, f, g)
+                                
 
+ans = {}
 
+n = 0
 for a, b, c, d, e, f, g in getnum():
+    n += 1
+    x = symbols('x')
+    eq1 = Eq((a/b)*((c*x)+d)+(e*x), (f*x)+g)
+    try:
+        solv, *_ = solveset(eq1, x).args
+        solv = float(solv)
+    except:
+        pass
+    solv = round(solv, 2)
+    print(f"{n} - {str(solv).ljust(5)} {a} {b} {c} {d} {e} {f} {g}", end="\r")
+    ans[solv] = [a, b, c, d, e, f, g]
+    
 
-    solv = solveset(Eq((a/b)(cx+d)+ex, fx+g), x)
-    print(solve)
-    input()
+print("All answers calculated...")
+ans = dict(sorted(ans.items(), key=lambda x: x[0], reverse=True))
+print("Sorted answers...")
+
+with open("mathsort.txt", "w") as file:
+    for x, (a, b, c, d, e, f, g) in ans.items():
+        
+        dist = f"{a} {b} {c} {d} {e} {f} {g}"
+        print(f"{dist.rjust(20)} - {x}")
+        file.write(f"{str(x).ljust(6)} - {a} {b} {c} {d} {e} {f} {g}")
+    
