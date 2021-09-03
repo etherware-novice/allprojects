7e6fc505a22fb5b0a506021bf70b2873ecbd65ca
etherware-novice <reesesimonasher@gmail.com>
2021-09-03 04:58:40 +0000

    added win condition

--- python/pbcom.py
+++ python/pbcom.py
@@ -1,18 +1,18 @@
 import curses, random
 from curses.textpad import rectangle
 
-global row, col
+global row, col, curbar
 
 def comwindow(screen):
 
     row, col = screen.getmaxyx()
     spawn_ul = (4, int(col/2) - 20)
-    spawn_lr = (row - 5, int(col/2) - 2)
+    spawn_lr = (25, int(col/2) - 2)
     rectangle(screen, *spawn_ul, *spawn_lr)
 
 
     bar_ul = (4, int(col/2) + 2)
-    bar_lr = (row - 5, int(col/2) + 20)
+    bar_lr = (25, int(col/2) + 20)
     rectangle(screen, *bar_ul, *bar_lr)
 
     return ((*spawn_ul, *spawn_lr), (*bar_ul, *bar_lr))
@@ -20,6 +20,8 @@ def comwindow(screen):
 
 @curses.wrapper
 def main(screen):
+
+    global curbar
 
     curses.raw(True)
     curses.cbreak(True)
@@ -39,7 +41,9 @@ def main(screen):
     cury = spw_cord[0] - 1
     timedely = 0
     curbar = []
-    while (c := screen.getch()) != 113: #ascii for q
+    while True: #ascii for q
+        c = screen.getch()
+        if c == 113: break
 
         if c in (68, 100, 261):
             if curseg == "RED":
@@ -48,7 +52,7 @@ def main(screen):
                 screen.addstr(row - 1, 0, "GAME OVER")
                 screen.refresh()
                 curses.napms(5000)
-                exit()
+                return
 
             tmp = cury - spw_cord[0] #look idk why it forces me to do this
             if 21 - tmp >= len(curbar): #if the piece doesnt "crash"
@@ -76,14 +80,20 @@ def main(screen):
                 screen.addstr(cury, spw_cord[1] + 1, "000".center(17))
 
             curseg = ""
+            if len(curbar) >= 20: return
 
         if c == 97: #lowercase a
+            curseg = ""
+            curbar = ["Blue", "Blue", "Blue", "Blue", "Blue", "Blue", "Blue", "Blue", "Blue", "Blue", "Blue", "Blue", "Blue", "Blue", "Blue", "Blue", "Blue", "Blue", "Blue"]
+
+        if c == 98:
             curseg = ""
 
         for n, x in enumerate(curbar):
             screen.addstr(bar_cord[2] - n - 1, bar_cord[1] + 1, f"{x}_{20-n}".center(17))
 
-        if timedely != 4000 * 2: 
+        #if timedely != 4000 * 2: 
+        if timedely != 500000:
             timedely += 1
             continue
         else: timedely = 0
@@ -119,7 +129,38 @@ def main(screen):
 
 
 
+if len(curbar) >= 20: print("YOU WIN")
+else: print("Better luck next time...")
 
+print()
+for x in ("Blue", "Yellow"):
+    print(f"{x.rjust(10)} - {curbar.count(x) * 5}%")
+
+pbbar = [1 if x == "Blue" else 0 for x in curbar]
+
+if pbbar.count(1) >= 20: print("Perfectionst Bonus\n")
+if pbbar.count(0) >= 20: print("Noncomformist Bonus\n")
+if pbbar == [1 for i in range(19)] + [0]: print("95% Bonus\n")
+if pbbar == [0 for i in range(19)] + [1]: print("Noncomformist 95% Bonus\n")
+if pbbar.count(1) == pbbar.count(0): print("50/50 Bonus\n")
+
+pattern = [1, 0] * 10
+altpattern = [0, 1] * 10
+
+if pbbar == pattern or pbbar == altpattern: print("Pattern Bonus\n")
+if pbbar == [1, 1, 0, 0] * 5 or pbbar == [0, 0, 1, 1] * 5: print("Pattern x2 Bonus\n")
+if pbbar == [1, 1, 1, 0, 0, 0] * 3 + [1, 1] or pbbar == [0, 0, 0, 1, 1, 1] * 3 + [0, 0]: print("Pattern x3 Bonus\n")
+if pbbar == [1, 1, 1, 1, 0, 0 ,0 ,0] * 2 + [1, 1, 1, 1] or pbbar == [0, 0, 0, 0, 1, 1, 1, 1] *2 + [0, 0, 0, 0]: print("Pattern x4 Bonus\n")
+if pbbar == [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0 ,0 ,0 ,0 ,0]: print("95 in morse code..Bonus\n")
+
+
+try:
+    if pbbar[:9] == pbbar[10:].reverse(): print("Symmetry Bonus\n")
+except: pass
+
+print(pbbar)
+
+print()
 
 
 
